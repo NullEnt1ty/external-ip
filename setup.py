42ce3d8a776d561e5f3795a2832c53eaa7b80805
Dominique Mattern <dominique.mattern@5minds.de>
2020-05-13 10:15:56 +0200

    Add install requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = requirements_file.read().splitlines()
 
 setuptools.setup(
     name='nullent1ty-external-ip',
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/NullEnt1ty/external-ip",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
